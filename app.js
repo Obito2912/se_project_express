b3608b42b66a5fbcac513d0a906bf38bffa476e5
Ovinson Lugo <ovinsonlugo12@gmail.com>
2025-08-02 20:01:06 -0600

    Add /crash-test endpoint in app.js to simulate crash

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(requestLogger);
 app.post("/signup", validateUserBody, createUser);
 app.post("/signin", validateAuthentication, login);
